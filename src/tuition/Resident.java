3dd60656c197e3ac3478c8fe6ce92d9875dd35c9
Mervin-James <43912182+Mervin-James@users.noreply.github.com>
2021-10-09 14:38:39 -0400

    revised methods in Student.java superclass and Resident.java subclass

--- src/tuition/Resident.java
+++ src/tuition/Resident.java
@@ -2,8 +2,16 @@ package tuition;
 
 public class Resident extends Student {
     private static final float MAX_FINANCIAL_AID = 10000;
+    private static final float FULL_TIME_TUITION_FEE = 12536;
+    private static final float RATE_PER_CREDIT_HOUR = 404;
+    private static final int FULL_TIME_BASE_RATE_MAX_CREDITS = 16;
+    private static final int FULL_TIME_BASE_RATE_MIN_CREDITS = 12;
+    private static final float PART_TIME_UNIVERSITY_FEE_MULTIPLIER = 0.8f;
     private float financialAid;
-    private float tuition;
+    private  float tuition;
+    private  float totalPayment;
+    private  Date lastPaymentDate;
+    private float tuitionDue;
 
     public Resident() {
 
@@ -19,6 +27,21 @@ public class Resident extends Student {
 
 
     @Override
-    public void tuitionDue() {}
+    public void tuitionDue() {
+        int numCredits = super.getCredits();
+        if (numCredits >= FULL_TIME_BASE_RATE_MAX_CREDITS) {
+            tuitionDue =
+                    FULL_TIME_TUITION_FEE + UNIVERSITY_FEE +
+                            RATE_PER_CREDIT_HOUR * (numCredits -
+                                    FULL_TIME_BASE_RATE_MAX_CREDITS);
+        } else if (numCredits >= FULL_TIME_BASE_RATE_MIN_CREDITS) {
+            tuitionDue = FULL_TIME_TUITION_FEE + UNIVERSITY_FEE;
+        } else {
+            tuitionDue =
+                    RATE_PER_CREDIT_HOUR * numCredits +
+                            PART_TIME_UNIVERSITY_FEE_MULTIPLIER *
+                                    UNIVERSITY_FEE;
+        }
+    }
 
 }
--- src/tuition/Student.java
+++ src/tuition/Student.java
@@ -1,40 +1,66 @@
 package tuition;
 
 public class Student {
-    private Profile profile;
-    private float tuition;
+    protected static final float UNIVERSITY_FEE = 3268;
+    private final Profile profile;
     private int credits;
+//    private final float tuition;
+//    private final float totalPayment;
+//    private final Date lastPaymentDate;
+//    private float tuitionDue;
 
     public Student(String name, Major major) {
         Profile profile = new Profile(name, major);
         this.profile = profile;
+//        this.tuition = 0;
+//        this.totalPayment = 0;
+        //TODO: See if this is valid
+//        this.lastPaymentDate = new Date("--/--/--");    //empty date
+//        this.tuitionDue = 0;
     }
 
-    public void tuitionDue() {}
+    public void tuitionDue() {
+    }
 
     @Override
     public String toString() {
-        return "";
+        return profile.toString() + ":" + credits + " credit hours" +
+                ":" + "tuition due:" + tuitionDue + ":" + "total " +
+                "payment:" +totalPayment + ":" + "last payment date: " +
+                lastPaymentDate;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(obj instanceof Student) {
+        if (obj instanceof Student) {
             Student student = (Student) obj;
             return student.profile.equals(this.profile);
         }
         return false;
     }
 
-    public Profile getProfile() {
-        return this.profile;
+//    public Profile getProfile() {
+//        return this.profile;
+//    }
+    public int getCredits() {
+//        return this.credits;
     }
 
     public float getTuition() {
-        return this.tuition;
+        return tuition;
     }
 
-    public int getCredits() {
-        return this.credits;
+//    public float getTotalPayment() {
+//        return this.totalPayment;
+//    }
+//
+//    public Date getLastPaymentDate() {
+//        return this.lastPaymentDate;
+//    }
+
+    public boolean calculateTuitionDue() {
+        tuitionDue = tuition - totalPayment;
+        return true;
     }
+
 }
