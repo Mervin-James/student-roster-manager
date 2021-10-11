39c1b4d28bd2c56ebf7068f7af3f40102ff6466c
Mervin-James <43912182+Mervin-James@users.noreply.github.com>
2021-10-11 00:18:04 -0400

    added JUnit test for add() in RosterTest.java

--- src/tuition/RosterTest.java
+++ src/tuition/RosterTest.java
@@ -19,49 +19,62 @@ public class RosterTest {
         Student[] sList = new Student[4];
         Roster roster = new Roster(sList);
 
-        //test case #1: adding student not in the roster should be valid.
+        //test case #1: adding mode students than the initial roster
+        // capacity should be valid.
+        Student student1 = new Student("Jane Doe", Major.BA);
+        assertTrue(roster.add(student1));
+        Student student2 = new Student("Joshua Patel", Major.BA);
+        assertTrue(roster.add(student2));
+        Student student3 = new Student("Sunny Lin", Major.BA);
+        assertTrue(roster.add(student3));
+        Student student4 = new Student("Barry Young", Major.BA);
+        assertTrue(roster.add(student4));
+        Student student5 = new Student("Mary Johnson", Major.BA);
+        assertTrue(roster.add(student5));
+
+        //test case #2: adding student not in the roster should be valid.
         Student student = new Student("Robert Johnson", Major.CS);
         assertTrue(roster.add(student));
 
-        //test case #2: adding student already in the roster should be
+        //test case #3: adding student already in the roster should be
         // invalid.
         assertFalse(roster.add(student));
 
-        //test case #3: adding Resident student not in the roster should be
+        //test case #4: adding Resident student not in the roster should be
         // valid.
         Student rStudent = new Resident("Jessica Smith", Major.CS, 15);
         assertTrue(roster.add(rStudent));
 
-        //test case #4: adding Resident student already in the roster should
+        //test case #5: adding Resident student already in the roster should
         // be invalid.
         assertFalse(roster.add(rStudent));
 
-        //test case #5: adding Nonresident student not in the roster should
+        //test case #6: adding Nonresident student not in the roster should
         // be valid.
         Student nrStudent = new NonResident("Kevin Ford", Major.CS, 15);
         assertTrue(roster.add(nrStudent));
 
-        //test case #6: adding Nonresident student already in the roster
+        //test case #7: adding Nonresident student already in the roster
         // should be invalid.
         assertFalse(roster.add(nrStudent));
 
-        //test case #7: adding Tristate student not in the roster should
+        //test case #8: adding Tristate student not in the roster should
         // be valid.
         Student tStudent = new TriState("Nate Reyes", Major.CS, 15, 4000,
                 State.NY);
         assertTrue(roster.add(tStudent));
 
-        //test case #8: adding Tristate student already in the roster should
+        //test case #9: adding Tristate student already in the roster should
         // be invalid.
         assertFalse(roster.add(tStudent));
 
-        //test case #9: adding International student not in the roster should
+        //test case #10: adding International student not in the roster should
         // be valid.
         Student iStudent = new International("Alex Jones", Major.CS, 15,
                 false);
         assertTrue(roster.add(iStudent));
 
-        //test case #10: adding International student already in the roster
+        //test case #11: adding International student already in the roster
         // should be invalid.
         assertFalse(roster.add(iStudent));
     }
